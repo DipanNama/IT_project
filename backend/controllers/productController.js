fdebc24dee3c777bfa73f955994a1b0529f6847b
Dipan Nama <dipannama91@gmail.com>
2023-01-21 03:19:25 +0530

    added error handler

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require("../models/productModels");
+const ErrorHandler = require("../utils/errorHandler");
 
 
 // Create Products - Admin
@@ -33,10 +34,7 @@ exports.getProductDetails = async (req, res,next) => {
     let product = await Product.findById(req.params.id);
 
     if (!product) {
-        return res.status(500).json({
-            success: false,
-            message: "Product not found"
-        })
+        return next(new ErrorHandler("Product Not Found",404));
     }
 
     res.status(200).json({
